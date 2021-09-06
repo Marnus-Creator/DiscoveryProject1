688ee71dafb3540b715d1ca5c6c2c3c7077aa202
Marnus-Creator <0827096007@Mb>
2021-09-06 21:20:15 +0200

    Busy with DB entities
    Created AccountType.java and AccountTransaction.java classes, but still busy with it (needs foreign key fixes)

--- AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/persistence/AccountTransaction.java
+++ AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/persistence/AccountTransaction.java
@@ -1,4 +1,108 @@
 package za.ac.nwu.ac.domain.persistence;
 
-public class AccountTransaction {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+//Fix Foreign key stuffies!!!!!!!!!!!!!!!!!!!
+
+@Entity
+@Table(name = "DEMO_ACCOUNT_TYPE", schema = "MARNUS")
+public class AccountTransaction implements Serializable{
+
+    private static final long serialVersionUID = 1199041377884282633L;
+
+    @Id
+    @SequenceGenerator(name = "VIT_RSA_GENERIC_SEQ", sequenceName = "MARNUS.VIT_RSA_GENERIC_SEQ", allocationSize = 1) //sequenceName
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "VIT_RSA_GENERIC_SEQ")
+    @Column(name = "TX_ID") //Primary key column
+    private long transactionID;
+
+    @Column(name = "ACCOUNT_TYPE_ID")
+    private long accountTypeId;
+
+    @Column(name = "MEMBER_ID")
+    private long memberId;
+
+    @Column(name = "AMOUNT")
+    private long amount;
+
+    @Column(name = "TX_DATE")
+    private long transactionDate;
+
+    public AccountTransaction(long transactionID, long accountTypeId, long memberId, long amount, long transactionDate) {
+        this.transactionID = transactionID;
+        this.accountTypeId = accountTypeId;
+        this.memberId = memberId;
+        this.amount = amount;
+        this.transactionDate = transactionDate;
+    }
+
+    public AccountTransaction() {
+    }
+
+    public long getTransactionID() {
+        return transactionID;
+    }
+
+    public void setTransactionID(long transactionID) {
+        this.transactionID = transactionID;
+    }
+
+    public long getAccountTypeId() {
+        return accountTypeId;
+    }
+
+    public void setAccountTypeId(long accountTypeId) {
+        this.accountTypeId = accountTypeId;
+    }
+
+    public long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(long memberId) {
+        this.memberId = memberId;
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(long amount) {
+        this.amount = amount;
+    }
+
+    public long getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(long transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AccountTransaction that = (AccountTransaction) o;
+        return transactionID == that.transactionID && accountTypeId == that.accountTypeId && memberId == that.memberId && amount == that.amount && transactionDate == that.transactionDate;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(transactionID, accountTypeId, memberId, amount, transactionDate);
+    }
+
+    @Override
+    public String toString() {
+        return "AccountTransaction{" +
+                "transactionID=" + transactionID +
+                ", accountTypeId=" + accountTypeId +
+                ", memberId=" + memberId +
+                ", amount=" + amount +
+                ", transactionDate=" + transactionDate +
+                '}';
+    }
 }
--- AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/persistence/AccountType.java
+++ AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/persistence/AccountType.java
@@ -6,20 +6,21 @@ import jdk.vm.ci.meta.Local;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
-@Table(name = "DEMO_ACCOUNT_TYPE", schema = "VITRSA_SANDBOX")     //Table name & Username (Schema migh not be needed)
+@Table(name = "DEMO_ACCOUNT_TYPE", schema = "MARNUS")     //Table name & Username (Schema might not be needed)
 
 
 public class AccountType implements Serializable {
 
-    //private static final long serialVersionUID = .....?????
-
-    @Id
-    @SequenceGenerator(name = "VIT_RSA_GENERIC_SEQ", sequenceName = "VITRSA_SANDBOX.VIT_RSA_GENERIC_SEQ", allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "VIT_RSA_GENERIC_SEQ")
+    private static final long serialVersionUID = 3866606557126890054L;
 
     //Columns in Table follow:
+    //This Column is a primary column and the keys are generated with this sequence Generator
+    @Id
+    @SequenceGenerator(name = "VIT_RSA_GENERIC_SEQ", sequenceName = "MARNUS.VIT_RSA_GENERIC_SEQ", allocationSize = 1)       //SequenceName =?
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "VIT_RSA_GENERIC_SEQ")
     @Column(name = "ACCOUNT_TYPE_ID") //Primary key column
     private long accountTypeID;
 
@@ -29,9 +30,78 @@ public class AccountType implements Serializable {
     @Column(name = "ACCOUNT_TYPE_NAME") //accountTypeName column
     private long accountTypeName;
 
-    // @JsonSerialize(using = LocalDateSerialiser.class)
+    //These Date conventions are for services (can remove it here):
+    // @JsonSerialize(using = LocalDateSerializer.class)
     //@JsonDeserialize(using = LocalDateDeserializer.class)
-    @Column(name = "CREATION_DATE")
-    private LocalDate creationDate;
 
+    @Column(name = "CREATION_DATE")
+    private LocalDate creationDate; //use this date not something like utilDate
+
+
+    //Use right-click > generate to generate public classes for each column (Revise DB entities vid at 12:30)
+
+    public AccountType(long accountTypeID, long mnemonic, long accountTypeName, LocalDate creationDate) {
+        this.accountTypeID = accountTypeID;
+        this.mnemonic = mnemonic;
+        this.accountTypeName = accountTypeName;
+        this.creationDate = creationDate;
+    }
+
+    public AccountType() {
+    }
+
+    public long getAccountTypeID() {
+        return accountTypeID;
+    }
+
+    public void setAccountTypeID(long accountTypeID) {
+        this.accountTypeID = accountTypeID;
+    }
+
+    public long getMnemonic() {
+        return mnemonic;
+    }
+
+    public void setMnemonic(long mnemonic) {
+        this.mnemonic = mnemonic;
+    }
+
+    public long getAccountTypeName() {
+        return accountTypeName;
+    }
+
+    public void setAccountTypeName(long accountTypeName) {
+        this.accountTypeName = accountTypeName;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AccountType that = (AccountType) o;
+        return accountTypeID == that.accountTypeID && mnemonic == that.mnemonic && accountTypeName == that.accountTypeName && Objects.equals(creationDate, that.creationDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountTypeID, mnemonic, accountTypeName, creationDate);
+    }
+
+    @Override
+    public String toString() {
+        return "AccountType{" +
+                "accountTypeID=" + accountTypeID +
+                ", mnemonic=" + mnemonic +
+                ", accountTypeName=" + accountTypeName +
+                ", creationDate=" + creationDate +
+                '}';
+    }
 }
