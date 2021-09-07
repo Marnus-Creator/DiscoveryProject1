d648ff8e2a471c30ea4fdb31b1106322fe7692fa
Marnus-Creator <0827096007@Mb>
2021-09-07 13:59:21 +0200

    Finished basic Repository Setup

--- AccountSystemRepository/src/main/java/za/ac/nwu/ac/repo/persistence/AccountTypeRepository.java
+++ AccountSystemRepository/src/main/java/za/ac/nwu/ac/repo/persistence/AccountTypeRepository.java
@@ -1,13 +1,11 @@
 package za.ac.nwu.ac.repo.persistence;
 
-//Watch repo setup vid!!!!!!!!!!!!!!!!!
-
-//import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import za.ac.nwu.ac.domain.persistence.AccountType;
 
-//Watch repo setup vid!!!!!!!!!!!!!!!!!
+
 
 @Repository
-public interface AccountTypeRepository {
+public interface AccountTypeRepository extends JpaRepository<AccountType, Long>{
 }
