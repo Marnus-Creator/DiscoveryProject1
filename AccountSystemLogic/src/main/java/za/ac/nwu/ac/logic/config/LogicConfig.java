97145b28edff23f2c75cfc2b938583f926355762
Marnus-Creator <0827096007@Mb>
2021-09-19 20:52:58 +0200

    Spring End-To-End started
    AccountTypeTranslatorImpl added
    FetchAccountTypeFlow added
    FetchAccountTypeFlowImpl added
    LogicConfig Modified
    TranslatorConfig added
    
    Edited AccountTypeController.java with AcountTypeDto and List
    Edited LogicConfig.java with @ComponentScan
    Edited WebConfig.java with @ComponentScan

--- AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/config/LogicConfig.java
+++ AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/config/LogicConfig.java
@@ -1,11 +1,14 @@
 package za.ac.nwu.ac.logic.config;
 
-
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import za.ac.nwu.ac.translator.config.TranslatorConfig;
 
 @Import({TranslatorConfig.class})
 @Configuration
+@ComponentScan(basePackages = {
+        "za.ac.nwu.ac.logic.flow"
+})
 public class LogicConfig {
 }
--- AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/flow/impl/FetchAccountTypeFlowImpl.java
+++ AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/flow/impl/FetchAccountTypeFlowImpl.java
@@ -1,4 +1,35 @@
 package za.ac.nwu.ac.logic.flow.impl;
 
-public class FetchAccountTypeFlowImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import za.ac.nwu.ac.domain.dto.AccountTypeDto;
+import za.ac.nwu.ac.logic.flow.FetchAccountTypeFlow;
+import za.ac.nwu.ac.translator.config.impl.AccountTypeTranslatorImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import za.ac.nwu.ac.domain.dto.AccountTypeDto;
+import za.ac.nwu.ac.logic.flow.FetchAccountTypeFlow;
+import za.ac.nwu.ac.translator.AccountTypeTranslator;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Transactional
+@Component
+public class FetchAccountTypeFlowImpl implements FetchAccountTypeFlow {
+    private final AccountTypeTranslator accountTypeTranslator;
+
+    @Autowired
+    public FetchAccountTypeFlowImpl(AccountTypeTranslator accountTypeTranslator)
+    {
+        this.accountTypeTranslator = accountTypeTranslator;
+    }
+
+    @Override
+    public List<AccountTypeDto> getAllAccountTypes()
+    {
+        return accountTypeTranslator.getAllAccountTypes();
+    }
+
 }
