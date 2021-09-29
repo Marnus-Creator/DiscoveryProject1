d35e6b5c25ceff71badc1072b470905625643225
Marnus-Creator <0827096007@Mb>
2021-09-30 00:48:30 +0200

    Started with How to unit test MVC 7:30
    Busy with edits in the AccountTypeController.java class.
    
    Need to create a modifyAccountTypeFlow

--- AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/AccountTypeController.java
+++ AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/AccountTypeController.java
@@ -20,7 +20,7 @@ import za.ac.nwu.ac.logic.flow.FetchAccountTypeFlow;
 import java.util.List;
 
 @RestController
-@RequestMapping("account-type")
+@RequestMapping("account-type")                                                         //There can only be one post, in this case the mapping is for account-type
 public class AccountTypeController {
 
     private final FetchAccountTypeFlow fetchAccountTypeFlow;
@@ -40,14 +40,14 @@ public class AccountTypeController {
             @ApiResponse(code = 400, message = "Sorry, Bad request", response = GeneralResponse.class),
             @ApiResponse(code = 404, message = "Not Found", response = GeneralResponse.class),
             @ApiResponse(code = 500, message = "Internal Server Error", response = GeneralResponse.class)})
-    public ResponseEntity<GeneralResponse<List<AccountTypeDto>>> getAll()
+    public ResponseEntity<GeneralResponse<List<AccountTypeDto>>> getAll()               //Has no input parameters
     {
         List<AccountTypeDto> accountTypes = fetchAccountTypeFlow.getAllAccountTypes();
         GeneralResponse<List<AccountTypeDto>> response = new GeneralResponse<>(true, accountTypes);
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return new ResponseEntity<>(response, HttpStatus.OK);                           //Returns a list of all the accountTypes
     }
 
-    @PostMapping("")
+    @PostMapping("")                                                                    //Gets received from the RequestMapping above
     @ApiOperation(value = "Creates a new AccountType.", notes = "Creates a new AccountType in the DataBase.")
     @ApiResponses(value = {
             @ApiResponse(code = 201, message = "AccountType was created successfully", response = GeneralResponse.class),
@@ -56,14 +56,14 @@ public class AccountTypeController {
     })
     public ResponseEntity<GeneralResponse<AccountTypeDto>> create(
             @ApiParam(value = "Request body to create new AcountType.", required = true)
-            @RequestBody AccountTypeDto accountType)
+            @RequestBody AccountTypeDto accountType)                                    //Need to request the body (input) to be displayed
     {
         AccountTypeDto accountTypeResponse = createAccountTypeFlow.create(accountType);
         GeneralResponse<AccountTypeDto> response = new GeneralResponse<>(true, accountTypeResponse);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
-    @GetMapping("{mnemonic}")
+    @GetMapping("{mnemonic}")                                                           //Making use of a path variable
     @ApiOperation(value = "Fetches the specified AccountType.", notes = "Fetches the new AccountType corresponding to the given mnemonic.")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Goal Found"),
@@ -76,11 +76,32 @@ public class AccountTypeController {
                     example = "MILES",
                     name = "mnemonic",
                     required = true)
-            @PathVariable("mnemonic") final String mnemonic)
+            @PathVariable("mnemonic") final String mnemonic)                            //Typically a mandatory PathVariable
     {
         AccountTypeDto accountType = fetchAccountTypeFlow.getAccountTypeByMnemonic(mnemonic);
         GeneralResponse<AccountTypeDto> response = new GeneralResponse<>(true, accountType);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
+
+ /*   @DeleteMapping("{mnemonic}")
+    @ApiOperation(value = "Deletes the specified AccountType.", notes = "Deletes the AccountType corresponding to the given mnemonic")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "AccountType deleted"),
+            @ApiResponse(code = 400, message = "Sorry, Bad request", response = GeneralResponse.class),
+            @ApiResponse(code = 404, message = "The Resource is NOT FOUND", response = GeneralResponse.class),
+            @ApiResponse(code = 500, message = "Internal Server Error", response = GeneralResponse.class)
+    })
+    public ResponseEntity<GeneralResponse<AccountTypeDto>> deleteAccountType(
+            @ApiParam(value = "The mnemnonic that uniquely idendtifies the AccountType.",
+                    example = "MILES",
+                    name = "mnemonic",
+                    required = true)
+            @PathVariable("mnemonic") final String mnemonic)
+    {
+        AccountTypeDto accountType = modifyAccountTypeFlow.deleteAcountType(mnemonic);      //Todo need to create modifyAccountTypeFlow
+        GeneralResponse<AccountTypeDto> response = new GeneralResponse<>(true, accountType);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }*/
+
 
 }
